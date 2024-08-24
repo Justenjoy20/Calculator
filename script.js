fb59bb00c8874461c3294c9c4ec04356da5bde0b
Justenjoy20 <prohor.ruslan068@gmail.com>
2024-08-24 15:59:24 +0200

    Done

--- script.js
+++ script.js
@@ -44,39 +44,60 @@ function divide(a, b) {
   return division;
 }
 
-/* Needs to call the operation , and to use first and second numbers*/
-function operate(operator, firstnumber, secondnumber) {
-  if ((operator = `+`)) {
+/* Need to call the operation , and to use first and second numbers*/
+function operate( firstnumber, operator, secondnumber) {
+  if ((operator === `+`)) {
     return add(firstnumber, secondnumber);
-  } else if ((operator = `-`)) {
+  } else if ((operator === `-`)) {
     return subtract(firstnumber, secondnumber);
-  } else if ((operator = `*`)) {
+  } else if ((operator ===`*`)) {
     return multiply(firstnumber, secondnumber);
-  } else if ((operator = `/`)) {
-    return division(firstnumber, secondnumber);
+  } else if ((operator === `/`)) {
+    return divide(firstnumber, secondnumber);
   }
 }
-//Function for displaying numbers on the screen
+// Storing the first number and second number and  the operation that user selects
+let firstnumb = "";
+let operation = "";
+let secondnumb = "";
+ 
+// append numbers
+function appendNumber (number)
+ { if(operation === ""){firstnumb = firstnumb + number; screen.value = firstnumb;} else {secondnumb = secondnumb + number; screen.value = secondnumb}}
+// displaying numbers on the screen and storing the value
+function typeTheValue () {
+  numb9.addEventListener(`click`,()=>{appendNumber(9); });
+  numb8.addEventListener(`click`, () => {appendNumber(8);  });
+  numb7.addEventListener(`click`, () => {appendNumber(7) ; });
+  numb6.addEventListener(`click`, () => {appendNumber(6);  });
+  numb5.addEventListener(`click`, () => {appendNumber(5);});
+  numb4.addEventListener(`click`, () => {appendNumber(4); });
+  numb3.addEventListener(`click`, () => {appendNumber(3); });
+  numb2.addEventListener(`click`, () => {appendNumber(2); });
+  numb1.addEventListener(`click`, () => {appendNumber(1);});
+  numb0.addEventListener(`click`, () => {appendNumber(0);});
+  comma.addEventListener(`click`, () => {appendNumber("."); });
+// maybe the operations will not work
+  operation1.addEventListener(`click`, () => {operation= "+" ; screen.value = operation});
+  operation2.addEventListener(`click`, () => {operation= "-" ; screen.value = operation });
+  operation3.addEventListener(`click`, () => {operation= "*" ; screen.value = operation });
+  operation4.addEventListener(`click`, () => {operation= "/" ; screen.value = operation });
+  equal.addEventListener(`click`, () => {giveTheResult();  })
+  clear.addEventListener(`click`,() => { clearTheScreen()});
+}
+ 
+typeTheValue ();
 
-function displayScreen () {numb9.addEventListener(`click`,()=>{screen.innerHTML = `9`});
-numb8.addEventListener(`click`, () => {screen.innerHTML = `8`});
-numb7.addEventListener(`click`, () => {screen.innerHTML = `7`});
-numb6.addEventListener(`click`, () => {screen.innerHTML = `6`});
-numb5.addEventListener(`click`, () => {screen.innerHTML = `5`});
-numb4.addEventListener(`click`, () => {screen.innerHTML = `4`});
-numb3.addEventListener(`click`, () => {screen.innerHTML = `3`});
-numb2.addEventListener(`click`, () => {screen.innerHTML = `2`});
-numb1.addEventListener(`click`, () => {screen.innerHTML = `1`});
-numb0.addEventListener(`click`, () => {screen.innerHTML = `0`});
-comma.addEventListener(`click`, () => {screen.innerHTML = `.`});
-operation1.addEventListener(`click`, () => {screen.innerHTML = `+`});
-operation2.addEventListener(`click`, () => {screen.innerHTML = `-`});
-operation3.addEventListener(`click`, () => {screen.innerHTML = `*`});
-operation4.addEventListener(`click`, () => {screen.innerHTML = `/`});
-equal.addEventListener(`click`, () => {screen.innerHTML = `=`});
-clear.addEventListener(`click`,() => {screen.innerHTMl = ``/*this one don't work */})}
-
-displayScreen ()
-// Storing the display value into a variable
-let tracker = ``
+// give the result
+let result = operate(firstnumb,operation,secondnumb);
+function giveTheResult () {if(firstnumb !== "" &&  operation !== "" && secondnumb !== "" )
+  {firstnumb = parseFloat(firstnumb);secondnumb = parseFloat(secondnumb);
+    result = operate(firstnumb,operation,secondnumb);
+     screen.value = result.toString();} 
+    firstnumb = result.toString();
+    secondnumb = "";
+    operation = "";
+}
+// clear the screen
+function clearTheScreen () {if(result !== "") {firstnumb = ""; operation= ""; secondnumb = ""; screen.value=""}}
 
